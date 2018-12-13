a00838691e7ae8b9c0cc153dc68942cbf4c7da32
pm38702003 <pm38702003@yahoo.com>
2018-12-12 19:15:23 -0500

    added final files and refactoring

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,9 @@ function renderButtons() {
         var button = $('<button>');
         button.text(breeds[i]);
         button.addClass("dogs");
-        button.addClass("btn btn-primary")
+        button.addClass("btn btn-primary");
+        button.addClass("ml-2");
+        button.addClass("mt-2")
 
         $("#dog-view").append(button);   // add buttons
     }
@@ -32,33 +34,47 @@ function playStopGifs() {  // swap urls
     //alert( stillGif + " 2  " +   movingGif )
 }
 
-
 function showDogs() {
     var breed = $(this).text();
     $("#dog-gifs").empty();
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + breed + "&api_key=THl5OhDrKR3D0ABoY3RHg8f7VMusLuQ7&limit=1&rating=g";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + breed + "&api_key=THl5OhDrKR3D0ABoY3RHg8f7VMusLuQ7&limit=10";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-
+        var appendRow;
         for (var i = 0; i < response.data.length; i++) {
+
+            if (i == 0 || (i) % 3 == 0)  // create new row at beginning and every 3rd item
+            {
+                appendRow = $("<div class='row'></div>").appendTo("#dog-gifs");
+            }
+
             var temp = response.data[i];
+            console.log(response)
+
+            var colBS = $("<div class='col-sm'>  </div>").appendTo(appendRow); //create new column everytime and append to row
 
             var stillGifUrl = (temp.images["fixed_height_still"].url).replace(/^http:\/\//i, 'https://');;
             var movingGifUrl = (temp.images["fixed_height"].url).replace(/^http:\/\//i, 'https://');
 
+            var animalDiv = $("<Div>");
+            var p = $("<p>")
+            p.text(temp.rating);
             var img = $('<img />');
             img.attr("src", stillGifUrl);           // static image     
             img.attr("data-name-1", stillGifUrl);    // moving image
             img.attr("data-name-2", movingGifUrl);
             img.addClass("dogGifs");
-            $("#dog-gifs").append(img);
 
-            console.log(img)
+            animalDiv.append(p);
+            animalDiv.append(img);
+
+            colBS.append(animalDiv); // append animal div to every column no matter what
         }
+
     });
 };
 
